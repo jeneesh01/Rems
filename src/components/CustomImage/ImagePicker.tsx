7bb560be3b5225599de45a761182e837ffe6454a
jeneesh <jeneesh.codeleap01@gmail.com>
2024-04-17 12:41:23 +0530

    image uploaded successs

--- src/components/CustomImage/ImagePicker.tsx
+++ src/components/CustomImage/ImagePicker.tsx
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/react-native';
-import React, {memo, useState} from 'react';
+import React, {memo, useEffect, useState} from 'react';
 import {
   Text,
   TouchableOpacity,
@@ -13,14 +13,35 @@ import {
 } from 'react-native-image-picker';
 import Modal from 'react-native-modal';
 import {styles} from './styles';
-import { RootState, useAppDispatch } from '../../redux/app/store';
-import { addPropertyData } from '../../redux/action/form';
-
+import {RootState, useAppDispatch} from '../../redux/app/store';
+import {addPropertyData} from '../../redux/action/form';
+import storage from '@react-native-firebase/storage';
+import {showMessage} from 'react-native-flash-message';
 type props = {
-  showModal:boolean;
-  setShowModal:(a:boolean)=>void;
+  showModal: boolean;
+  setShowModal: (a: boolean) => void;
 };
-const AddPhoto = ({showModal,setShowModal}:props) => {
+const AddPhoto = ({showModal, setShowModal}: props) => {
+  const uplaodimage = async (fileName: any, url: string) => {
+    try {
+      await storage().ref(fileName).putFile(url);
+      dispatch(addPropertyData('property_images', [fileName]));
+      showMessage({
+        message: 'image uploaded successfully',
+        type: 'success',
+      });
+    } catch (error) {
+      showMessage({
+        message: 'Error in uploading image',
+        type: 'warning',
+      });
+      console.log('Errror', error);
+    } finally {
+      setShowModal(false);
+
+      console.log('over');
+    }
+  };
   const dispatch = useAppDispatch();
 
   const ontakephotopress = async () => {
@@ -40,11 +61,9 @@ const AddPhoto = ({showModal,setShowModal}:props) => {
       } else {
         if (response && response.assets && response.assets[0].uri) {
           const url = response.assets[0].uri;
-          
-          dispatch(addPropertyData('property_images',[url]));
-    setShowModal(false)
+          const fileName = url.split('/').pop();
 
-
+          uplaodimage(fileName, url);
         } else {
           console.log('Image URI is not available.');
         }
@@ -66,12 +85,10 @@ const AddPhoto = ({showModal,setShowModal}:props) => {
         console.error('ImagePicker Error: ', response.errorCode);
       } else {
         if (response && response.assets && response.assets[0].uri) {
-
           const url = response.assets[0].uri;
+          const fileName = url.split('/').pop();
 
-          dispatch(addPropertyData('property_images',[url]))
-    setShowModal(false)
-          
+          uplaodimage(fileName, url);
         } else {
           console.log('Image URI is not available.');
         }
@@ -79,46 +96,46 @@ const AddPhoto = ({showModal,setShowModal}:props) => {
     });
   };
   return (
-      <Modal
-        animationOutTiming={1000}
-        onRequestClose={() => setShowModal(false)}
-        backdropTransitionOutTiming={1000}
-        isVisible={showModal}
-        backdropOpacity={0.5}
-        style={{margin: 0}}>
-        <TouchableWithoutFeedback
-          style={{flex: 1}}
-          onPress={() => setShowModal(false)}>
-          <View style={{flex: 1, justifyContent: 'flex-end', marginBottom: 20}}>
-            <View style={styles.modalcontainerstyle}>
-              <TouchableOpacity
-                onPress={() => ontakephotopress()}
-                style={styles.tochableOpacitystyle}>
-                <View style={styles.modaltviewextstyle}>
-                  <Text style={styles.text}>Open Camera</Text>
-                </View>
-              </TouchableOpacity>
-              <View style={styles.midcontainer} />
-              <TouchableOpacity
-                onPress={() => choosefromgalary()}
-                style={styles.tochableOpacitystyle}>
-                <View style={styles.modaltviewextstyle}>
-                  <Text style={styles.text}>Choose From Gallary</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.cancelcontainerstyle}>
-              <TouchableOpacity
-                onPress={() => setShowModal(false)}
-                style={styles.tochableOpacitystyle}>
-                <View style={styles.modaltviewextstyle}>
-                  <Text style={styles.text}>Cancel</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
+    <Modal
+      animationOutTiming={1000}
+      onRequestClose={() => setShowModal(false)}
+      backdropTransitionOutTiming={1000}
+      isVisible={showModal}
+      backdropOpacity={0.5}
+      style={{margin: 0}}>
+      <TouchableWithoutFeedback
+        style={{flex: 1}}
+        onPress={() => setShowModal(false)}>
+        <View style={{flex: 1, justifyContent: 'flex-end', marginBottom: 20}}>
+          <View style={styles.modalcontainerstyle}>
+            <TouchableOpacity
+              onPress={() => ontakephotopress()}
+              style={styles.tochableOpacitystyle}>
+              <View style={styles.modaltviewextstyle}>
+                <Text style={styles.text}>Open Camera</Text>
+              </View>
+            </TouchableOpacity>
+            <View style={styles.midcontainer} />
+            <TouchableOpacity
+              onPress={() => choosefromgalary()}
+              style={styles.tochableOpacitystyle}>
+              <View style={styles.modaltviewextstyle}>
+                <Text style={styles.text}>Choose From Gallary</Text>
+              </View>
+            </TouchableOpacity>
           </View>
-        </TouchableWithoutFeedback>
-      </Modal>
+          <View style={styles.cancelcontainerstyle}>
+            <TouchableOpacity
+              onPress={() => setShowModal(false)}
+              style={styles.tochableOpacitystyle}>
+              <View style={styles.modaltviewextstyle}>
+                <Text style={styles.text}>Cancel</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
   );
 };
 
